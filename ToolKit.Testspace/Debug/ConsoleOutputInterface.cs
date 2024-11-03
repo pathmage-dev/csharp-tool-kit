fff2199388eaa1171ffe70ab49efc45e74e951dc
pathmage <pathmage@outlook.com>
2024-11-03 22:36:47 +0100

    ConsoleOutput multithreading test

--- ToolKit.Testspace/Debug/ConsoleOutputInterface.cs
+++ ToolKit.Testspace/Debug/ConsoleOutputInterface.cs
@@ -11,4 +11,29 @@ public class ConsoleOutputInterface
 
 		print(null, "", 'a');
 	}
+
+	public static void MultithreadingTest()
+	{
+		var threads = new Thread[4];
+
+		for (var i = 0; i < threads.Length; i++)
+		{
+			var ii = i;
+			threads[i] = new Thread(() => threadTest(ii));
+		}
+
+		for (var i = 0; i < threads.Length; i++)
+			threads[i].Start();
+	}
+
+	static void threadTest(int thread_id)
+	{
+		for (var i = 0; i < 10; i++)
+		{
+			print(thread_id, "Hello", "World!");
+
+			Random random = new();
+			Thread.Sleep(random.Next(0, 100));
+		}
+	}
 }
