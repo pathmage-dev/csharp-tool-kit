012087181f184c9ae7870f4d7eaf4e79e1ff8719
pathmage <pathmage@outlook.com>
2024-11-04 16:48:44 +0100

    Iteration extensions

---
+++ ToolKit.Testspace/Utilities/Extensions/Iteration.cs
@@ -0,0 +1,108 @@
+﻿namespace ToolKit.Testspace.Utilities.Extensions;
+
+public class Iteration
+{
+	public static void ForeachInt()
+	{
+		// foreach (var i in -1)
+		// 	print(i);
+
+		print("i in 0");
+		foreach (var i in 0)
+			print(i);
+
+		print("i in 1");
+		foreach (var i in 1)
+			print(i);
+
+		print("i in 10");
+		foreach (var i in 10)
+			print(i);
+
+		int[] values = [1, 2, 3, 4, 5];
+		print("i in values.Length");
+		foreach (var i in values.Length)
+			print(i, values[i]);
+	}
+
+	public static void ForeachIndex()
+	{
+		// foreach (var i in ^-1)
+		// print(i);
+
+		// foreach (var i in new Index(10))
+		// 	print(i);
+
+		print("i in ^0");
+		foreach (var i in ^0)
+			print(i);
+
+		print("i in ^1");
+		foreach (var i in ^1)
+			print(i);
+
+		print("i in ^10");
+		foreach (var i in ^10)
+			print(i);
+
+		int[] values = [1, 2, 3, 4, 5];
+		print("i in ^values.Length");
+		foreach (var i in ^values.Length)
+			print(i, values[i]);
+	}
+
+	public static void RangeTest()
+	{
+		var range = ..10;
+		print(range.Start.IsFromEnd, range.End.IsFromEnd);
+
+		range = 10..;
+		print(range.Start.IsFromEnd, range.End.IsFromEnd);
+	}
+
+	public static void ForeachRange()
+	{
+		// foreach (var i in ..-1)
+		// print(i);
+
+		print("i in ..0");
+		foreach (var i in ..0)
+			print(i);
+
+		print("i in ..1");
+		foreach (var i in ..1)
+			print(i);
+
+		print("i in ..10");
+		foreach (var i in ..10)
+			print(i);
+
+		int[] values = [1, 2, 3, 4, 5];
+		print("i in 2..values.Length");
+		foreach (var i in 2..values.Length)
+			print(i, values[i]);
+	}
+
+	public static void ForeachRangeBackwards()
+	{
+		// foreach (var i in -1..)
+		// print(i);
+
+		print("i in 0..");
+		foreach (var i in 0..)
+			print(i);
+
+		print("i in 1..");
+		foreach (var i in 1..)
+			print(i);
+
+		print("i in 10..");
+		foreach (var i in 10..)
+			print(i);
+
+		int[] values = [1, 2, 3, 4, 5];
+		print("i in values.Length..2");
+		foreach (var i in values.Length..2)
+			print(i, values[i]);
+	}
+}
---
+++ ToolKit/Utilities/Extensions/Iteration.cs
@@ -0,0 +1,62 @@
+﻿namespace ToolKit;
+
+partial class Extensions
+{
+	public static Iterator GetEnumerator(this int end_at)
+	{
+#if ERR
+		ArgumentOutOfRangeException.ThrowIfLessThan(end_at, 0);
+#endif
+		return new Iterator(end_at);
+	}
+
+	public ref struct Iterator(int end_at)
+	{
+		public int Current { get; private set; } = -1;
+		readonly int end_at = end_at;
+
+		public bool MoveNext() => ++Current < end_at;
+	}
+
+	public static ReverseIterator GetEnumerator(this Index start_before)
+	{
+#if ERR
+		ArgumentOutOfRangeException.ThrowIfEqual(start_before.IsFromEnd, false);
+#endif
+		return new ReverseIterator(start_before.Value);
+	}
+
+	public ref struct ReverseIterator(int start_before)
+	{
+		public int Current { get; private set; } = start_before;
+
+		public bool MoveNext() => -1 < --Current;
+	}
+
+	public static RangeIterator GetEnumerator(this Range range)
+	{
+		if (range.Start.Value < range.End.Value)
+		{
+#if ERR
+			ArgumentOutOfRangeException.ThrowIfEqual(range.Start.IsFromEnd, true);
+			ArgumentOutOfRangeException.ThrowIfEqual(range.End.IsFromEnd, true);
+#endif
+			return new RangeIterator(range.Start.Value - 1, range.End.Value, 1);
+		}
+
+#if ERR
+		ArgumentOutOfRangeException.ThrowIfEqual(range.Start.IsFromEnd, true);
+		ArgumentOutOfRangeException.ThrowIfEqual(!range.End.Equals(^0) && range.End.IsFromEnd, true);
+#endif
+		return new RangeIterator(range.Start.Value, range.End.Value - 1, -1);
+	}
+
+	public ref struct RangeIterator(int start_at, int end_before, int step_by)
+	{
+		public int Current { get; private set; } = start_at;
+		readonly int end_before = end_before;
+		readonly int step_by = step_by;
+
+		public bool MoveNext() => (Current += step_by) != end_before;
+	}
+}
