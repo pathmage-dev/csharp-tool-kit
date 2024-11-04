a5514513aa68f3dd7ab3fefaa88e34588f6b2bea
pathmage <pathmage@outlook.com>
2024-11-04 12:39:50 +0100

    ManagedConsoleOutput

---
+++ ToolKit.Testspace/Debug/ManagedConsoleOutput.cs
@@ -0,0 +1,14 @@
+﻿using ToolKit.Debug;
+
+namespace ToolKit.Testspace.Debug;
+
+public class ManagedConsoleOutputTest
+{
+	public static void Test()
+	{
+		Project.ConsoleOutput = new ManagedConsoleOutput(Console.WriteLine);
+
+		write('A');
+		print('B');
+	}
+}
---
+++ ToolKit/Debug/ManagedConsoleOutput.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ToolKit.Debug;
+
+public sealed class ManagedConsoleOutput : IConsoleOutput
+{
+	readonly StringBuilder output = new();
+
+	public Action<string> Write { get; }
+	public Action WriteLine { get; }
+
+	public ManagedConsoleOutput(Action<string> write_line)
+	{
+		Write = value => output.Append(value);
+
+		WriteLine = () =>
+		{
+			write_line(output.ToString());
+			output.Clear();
+		};
+	}
+}
